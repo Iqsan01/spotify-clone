6604e773f460b1e3adf6cae3d472c22a11884111
Muhammad Ikhsan <99802023+Iqsan01@users.noreply.github.com>
2023-05-08 15:47:19 +0700

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,8 +3,8 @@ import styled from "styled-components";
 
 export default function Login() {
   const handleClick = async () => {
-    const client_id = "fd881d4718b24b5a8c59e4e9ea08a766";
-    const redirect_uri = "https://clone-spotify-api.netlify.app/";
+    const client_id = "b10ac7cc459d474e961a6603c15da715";
+    const redirect_uri = "https://spotify-clone-api.netlify.app/";
     const api_uri = "https://accounts.spotify.com/authorize";
     const scope = [
       "user-read-private",
